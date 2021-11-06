d5c647e47ac7baf1cf702ce0d136476f7447386a
Mihir Bhawsar <mihir18bhawsar@gmail.com>
2021-11-06 11:02:20 +0530

    added a comment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
-
+//creating app
 const app = express();
 
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
