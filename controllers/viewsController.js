bb9d64935e3ac6ea692c905f764597a33b115768
Mihir Bhawsar <mihir18bhawsar@gmail.com>
2021-11-08 14:29:03 +0530

    content security policy change

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,10 +8,10 @@ exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' http://localhost:8000 https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com http://localhost:8000 https://js.stripe.com/v3/ https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    // .set(
+    //   'Content-Security-Policy',
+    //   "default-src 'self' http://localhost:8000 https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com http://localhost:8000 https://js.stripe.com/v3/ https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    // )
     .render('overview', {
       title: 'All Tours',
       tours
@@ -26,10 +26,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   if (!tour) next(new AppError('No Tour With This Name', 404));
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    // .set(
+    //   'Content-Security-Policy',
+    //   "default-src 'self' https://*.mapbox.com https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    // )
     .render('tour', {
       title: `${tour.name} Tour`,
       tour
@@ -39,10 +39,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.getLoginForm = catchAsync(async (req, res) => {
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' http://localhost:8000 https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com http://localhost:8000 https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    // .set(
+    //   'Content-Security-Policy',
+    //   "default-src 'self' http://localhost:8000 https://js.stripe.com/v3/ ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com http://localhost:8000 https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    // )
     .render('login', {
       title: 'Log into your account'
     });
