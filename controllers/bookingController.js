9a7cafe9507726ec54b9f836cab235e226f97584
Mihir Bhawsar <mihir18bhawsar@gmail.com>
2021-11-10 22:57:39 +0530

    bugfix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,7 +8,7 @@ const factory = require('./handlerFactory');
 async function createBookingEntry(session) {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 }
 
